bc5d4d5a0d6e6ba8ba22c6edfedb7abe7efc62ba
jaimecuellar14 <jamescuellar14@gmail.com>
2020-10-26 23:47:47 +0100

    Copiamos tabla de una bd a otra

--- index.php
+++ index.php
@@ -12,23 +12,8 @@
         "process:"
     );
     $data = getopt(" ",$values);
-    print_r($data);
     checkParams($data);
-    /*
-    try{
-        $pdo = new PDO($hostDB,$hostUser);
-        $query = $pdo->prepare("SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME=:dbname");
-        $query->execute(['dbname'=>$dbName]);
-        //$version = $query->fetch();
-
-        echo json_encode((bool) $query->fetchColumn());
-    }
-    catch(PDOException $e){
-        echo json_encode($e);
-    }
-
-*/
-
+  
     function checkParams($params){
         if(count($params)!=9){
             echo "Error en la ejecucion del script: Numero de parametros incorrectos";
@@ -51,8 +36,10 @@
 
          $conn2 = checkConnectionDestination($destinoHost,$destinoUser,$data["destinationDB"]);
 
-         if($conn1 AND $conn2){
+         if($conn1["result"] AND $conn2["result"]){
              print_r("buena conexion ambos");
+
+             copyTable($conn2["pdo"]);
          }
          else{
              print_r("ERROR CONEXION");
@@ -90,6 +77,21 @@
         $query = $pdo->prepare("SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME=:dbname");
         $query->execute(["dbname"=>$db]);
         //print_r((bool)$query->fetchColumn());
-        return ((bool)$query->fetchColumn());
+        return array("result"=>((bool)$query->fetchColumn()), "pdo"=>$pdo);
+    }
+
+    function copyTable($pdo){
+        $query = $pdo->prepare("CREATE TABLE test.usuarios LIKE supertech.usuarios");
+        if($query->execute()){
+            print_r("works");
+            $queryCopy = $pdo->prepare("INSERT test.usuarios SELECT * FROM supertech.usuarios");
+            if($queryCopy->execute()){
+                print_r("COPIO DATOS DE TABLA");
+            }else{
+                print_r("NO SE PUDO HACER COPIA");
+            }
+        }else{
+            print_r("ERROR COPIA DE TABLA");
+        }
     }
 ?>
